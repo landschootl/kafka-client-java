2002977d4d87afa7824e10c6f000379ac49ffabf
LANDSCHOOT LUDOVIC <ludovic.landschoot.partner@decathlon.com>
2019-03-15 14:58:50 +0100

    chore (init): create kakfka client

---
+++ src/main/java/KafkaClient.java
@@ -0,0 +1,54 @@
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Properties;
+
+public class KafkaClient {
+    String topic;
+    KafkaConsumer<String, String> consumer;
+    Producer<String, String> producer;
+
+    public KafkaClient() {
+        String brokers = System.getenv("KAFKA_BROKERS");
+        String username = System.getenv("KAFKA_USERNAME");
+        String password = System.getenv("KAFKA_PASSWORD");
+        client(brokers, username, password);
+    }
+
+    private void client(String brokers, String username, String password){
+        topic = username + "-default";
+        String jaasTemplate = "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"%s\" password=\"%s\";";
+        String jaasCfg = String.format(jaasTemplate, username, password);
+        String serializer = StringSerializer.class.getName();
+        String deserializer = StringDeserializer.class.getName();
+        Properties props = new Properties();
+        props.put("bootstrap.servers", brokers);
+        props.put("group.id", username + "-consumer");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("auto.offset.reset", "earliest");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.deserializer", deserializer);
+        props.put("value.deserializer", deserializer);
+        props.put("key.serializer", serializer);
+        props.put("value.serializer", serializer);
+        props.put("security.protocol", "SASL_SSL");
+        props.put("sasl.mechanism", "SCRAM-SHA-256");
+        props.put("sasl.jaas.config", jaasCfg);
+
+        consumer = new KafkaConsumer(props);
+        producer = new KafkaProducer(props);
+    }
+
+    public static void main(String[] args) {
+		KafkaClient c = new KafkaClient();
+    }
+}
